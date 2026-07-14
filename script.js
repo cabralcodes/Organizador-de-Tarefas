789ccf4458def87df5e153816765a4dba6906f56
Matheus Augusto <matheusaugustodev.web@gmail.com>
2026-07-14 16:36:35 -0300

    Fixing the task card bug

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 let ColunaAfazer = document.getElementById('IdContainerAfazer');
 let ColunaFazendo = document.getElementById('IdContainerFazendo');
 let ColunaFeito = document.getElementById('IdContainerFeito');
-
 let InputNomeTarefa = document.getElementById("IdNomeTarefa");
 let BtnAddTarefa = document.getElementById('IdBtnTarefa');
+let NomeTarefa = InputNomeTarefa;
 
 
 
 BtnAddTarefa.addEventListener('click', function(){
-    let Id = Date.now()
-    let IdCard = "Card- " +Id;
-    let NomeTarefa = InputNomeTarefa.value;
+    
+    if(NomeTarefa === '' || NomeTarefa.value.length <= 2){
+        InputNomeTarefa.style.borderColor = "red";
+        BtnAddTarefa.style.borderColor = "red";
+        BtnAddTarefa.style.color = "red";
+        Card.remove();  
+        
+    }
+    if(NomeTarefa.value.length >2){
+        BtnAddTarefa.style.borderColor = "#000";
+        BtnAddTarefa.style.color = "black";
+        InputNomeTarefa.style.borderColor = "black";
+    }       
         let Card = document.createElement('div');
         let NomeTarefaCard = document.createElement('p');
-        NomeTarefaCard.innerHTML = InputNomeTarefa.value
-        //NomeTarefaCard.innerText = NomeTarefa;
+        NomeTarefaCard.className = "TituloTarefa";
+        NomeTarefaCard.innerText = NomeTarefa.value;
         Card.className = 'CardTarefa';
         NomeTarefaCard.style.fontFamily = 'Nexa-Heavy';
-        Card.append(NomeTarefaCard);
+        let ContainerBtns = document.createElement('div');
+        let EditarBtn = document.createElement('button');
+        let ApagarBtn = document.createElement('button');
+        
+        let ImgEditar = document.createElement('img');
+        let ImgApagar = document.createElement('img');
+        ContainerBtns.className = "ContainerBotoes";
+        ApagarBtn.className = "BotaoEditarTarefa";
+        EditarBtn.className = "BotaoEditarTarefa";
+        ImgEditar.src = "./images/pencil_112448.png";
+        ImgApagar.src = "./images/rubbishbin_102620.png";
+        ImgEditar.alt = "BotaoDeEditar";
+        ImgApagar.alt = "BotaoDeApagar";
+        
+        EditarBtn.append(ImgEditar);
+        ApagarBtn.append(ImgApagar);
+        ContainerBtns.append(EditarBtn, ApagarBtn)
+        Card.append(NomeTarefaCard, ContainerBtns);
         ColunaAfazer.appendChild(Card)
         
-       
-    })
+        let Id = Date.now()
+        let IdCard = "Card- " +Id;
+        
+        
+        let Tarefas = {
+            Nome: NomeTarefaCard,
+            id: IdCard
+        }
+        
+})
+
 
