eec7a2a51e8685ae86e7b9bd3ffba2ecd4da3fc1
Matheus Augusto <matheusaugustodev.web@gmail.com>
2026-07-16 11:22:09 -0300

    Creating the task buttons and their logic

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ let ColunaFeito = document.getElementById('IdContainerFeito');
 let InputNomeTarefa = document.getElementById("IdNomeTarefa");
 let BtnAddTarefa = document.getElementById('IdBtnTarefa');
 let Tarefas = [];
-let NomeTarefa = InputNomeTarefa;
+
 
 
 
 BtnAddTarefa.addEventListener('click', function(){
     
-    if(NomeTarefa === '' || NomeTarefa.value.length <= 2){
+    if(InputNomeTarefa.value === '' || InputNomeTarefa.value.length <= 2){
         InputNomeTarefa.style.borderColor = "red";
         BtnAddTarefa.style.borderColor = "red";
         BtnAddTarefa.style.color = "red";
         Card.remove();  
         
     }
-    if(NomeTarefa.value.length >2){
+    if(InputNomeTarefa.value.length >2){
         BtnAddTarefa.style.borderColor = "#000";
         BtnAddTarefa.style.color = "black";
         InputNomeTarefa.style.borderColor = "black";
@@ -25,7 +25,7 @@ BtnAddTarefa.addEventListener('click', function(){
         let Card = document.createElement('div');
         let NomeTarefaCard = document.createElement('p');
         NomeTarefaCard.className = "TituloTarefa";
-        NomeTarefaCard.innerText = NomeTarefa.value;
+        NomeTarefaCard.innerText =InputNomeTarefa.value;
         Card.className = 'CardTarefa';
         NomeTarefaCard.style.fontFamily = 'Nexa-Heavy';
         let ContainerBtns = document.createElement('div');
@@ -53,17 +53,45 @@ BtnAddTarefa.addEventListener('click', function(){
         
         let NovaTarefa = {
             id: Id,
-            Nome: NomeTarefaCard,
+            Nome: InputNomeTarefa.value,
             IdCard: IdCard,
         }
         Tarefas.push(NovaTarefa);
 
+
+        EditarBtn.addEventListener('click', () => {
+            let CorpoEditar = document.createElement('div');
+            let EspacoEditar = document.createElement('textarea');
+            let BtnSalvar = document.createElement('button');
+
+            CorpoEditar.className = "ContainerEspacoEditar";
+            BtnSalvar.className = "SalvarNovoNome";
+
+            BtnSalvar.textContent = 'Salvar';
+            EspacoEditar.value = InputNomeTarefa.value;
+
+            CorpoEditar.append(EspacoEditar, BtnSalvar);
+           document.body.appendChild(CorpoEditar);
+
+
+           BtnSalvar.addEventListener('click', () => {
+            CorpoEditar.remove();
+            NomeTarefaCard.innerText = EspacoEditar.value;
+            NovaTarefa.Nome = EspacoEditar.value;
+
+
+           })
+
+        })
+
+
         ApagarBtn.addEventListener('click', () =>{
             Card.remove();
-            Tarefas.filter(item => item.id !== Id);
+            Tarefas = Tarefas.filter(item => item.id !== Id);
             console.log(Tarefas);
         })
         
-})
 
+
+})
 
